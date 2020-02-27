886aeead583fd929abbd9810872429c832e7a48f
dor <dorshamir4@gmail.com>
2020-02-27 19:49:56 +0200

    after sqlite and landscape

--- app/src/main/java/com/example/cleaningthecity/MainActivity.java
+++ app/src/main/java/com/example/cleaningthecity/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.cleaningthecity;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
